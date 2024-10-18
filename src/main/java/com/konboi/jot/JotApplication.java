2f2125469f3f6074007b509a09b18bdcd6782af5
Konboi <ryosuke.yabuki@gmail.com>
2024-10-18 23:33:54 +0900

    add controller for check

--- src/main/java/com/konboi/jot/JotApplication.java
+++ src/main/java/com/konboi/jot/JotApplication.java
@@ -2,8 +2,10 @@ package com.konboi.jot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.konboi.jot.controller")
 public class JotApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/konboi/jot/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.konboi.jot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+}
