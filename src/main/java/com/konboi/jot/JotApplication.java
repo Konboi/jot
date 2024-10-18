2a59ef838e8bfafd9bba5c44da1326e7d4755b38
Konboi <ryosuke.yabuki@gmail.com>
2024-10-18 22:38:42 +0900

    initial commit

---
+++ src/main/java/com/konboi/jot/JotApplication.java
@@ -0,0 +1,13 @@
+package com.konboi.jot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konboi/jot/JotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konboi.jot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
